6c275b4fb1009a88cf7a8e1263097b7008d6de9c
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2020-01-10 12:57:59 +0100

    remove bufferedstreams

--- rc.simple.jl
+++ rc.simple.jl
@@ -2,19 +2,16 @@ dir = dirname(dirname(@__FILE__))
 push!(LOAD_PATH, dir)
 
 using SeqJL
-using BufferedStreams
 
 function main(arg)
         f = open(arg)
-        o = BufferedOutputStream(stdout)
         total = 0
         for l in eachline(f)
            seq = reverse_complement!(Seq(l))
            total += length(seq)
-           println(o, seq)
+           println(seq)
         end
         close(f)
-        flush(o)
         println("$total")
 end
 
